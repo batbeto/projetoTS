9ff7007912f5d13c7368d4324c79c65940e6ac10
Adalberto Maia <btowmaia@gmail.com>
2023-08-18 15:26:01 -0300

    state

--- state-lvl/src/app/page.tsx
+++ state-lvl/src/app/page.tsx
@@ -4,15 +4,20 @@ import { useState } from "react";
 
 const Page = () =>{
   let [count, setCount] = useState<number>(0);
+  let [secret, setSecret] = useState<boolean>(false)
 
   const handleClick = () =>{
     setCount(count++);
+    setSecret(true);
   }
 
   return (
     <div className="w-screen h-screen flex flex-col justify-center items-center text-3xl">
       <p>{count}</p>
       <button className="bg-blue-500 p-3" onClick={handleClick}>+1</button>
+      {secret &&
+        <div className="p-1 bg-blue-300 rounded-md mt-3">Área secreta</div>
+      }      
     </div>
   );
 }
