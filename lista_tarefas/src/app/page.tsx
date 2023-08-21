7b083574cd099301ecbdc26e14073f8d983677c2
Adalberto Maia <btowmaia@gmail.com>
2023-08-21 16:40:07 -0300

    state

--- lista_tarefas/src/app/page.tsx
+++ lista_tarefas/src/app/page.tsx
@@ -1,9 +1,27 @@
 'use client'
 
+import { TodoItem } from "@/types/TodoItem";
 import { useState } from "react";
 
 const Page = () =>{
-  const [list, setList] = useState([]);
+  const [itemInput, setItemInput] =  useState('');
+  const [list, setList] = useState<TodoItem[]>([
+    {label: 'Fazer dever de casa', checked: false},
+    {label: 'Comprar o bolo', checked: false}
+  ]);
+  
+  const handleAddButton = () =>{
+    if(itemInput.trim() == '') return;
+    setList([
+      ...list,
+      {label: itemInput, checked: false}
+    ]);
+    setItemInput('');
+  };
+
+  const deleteItem = (index: number) =>{
+    setList(list.filter((item, key) => key != index));
+  }
 
   return (
     <div className="w-screen h-screen flex flex-col justify-center items-center text-3xl">
@@ -13,12 +31,15 @@ const Page = () =>{
           type="text"
           placeholder="O que deseja fazer?" 
           className="border border-black p-3 text-2xl text-black rounded-md mb-3"
+          value={itemInput}
+          onChange={e => setItemInput(e.target.value)}
         />
-        <button>Adicionar</button>
+        <button onClick={handleAddButton}>Adicionar</button>
       </div>
       <ul className="w-full max-w-lg list-disc pl-5">
-        <li>Tarefa especifica - <button className="hover:underline">[ Deletar ]</button></li>
-        <li>Tarefa especifica - <button className="hover:underline">[ Deletar ]</button></li>
+        {list.map((item, index) => (
+           <li key={index}>{item.label} - <button className="hover:underline" onClick={()=>deleteItem(index)}>[ Deletar ]</button></li> 
+        ))}
       </ul>      
     </div>
   );
