623375551d7611351af4a1be8ea4dfe1231836a9
Adalberto Maia <btowmaia@gmail.com>
2023-09-11 17:36:20 -0300

    reducer

--- reducer/src/app/page.tsx
+++ reducer/src/app/page.tsx
@@ -1,52 +1,66 @@
 'use client'
 
 import { listReducer } from "@/reducers/listReducer";
-import { useReducer } from "react";
+import { useReducer, useState } from "react";
 
 const Page = () =>{
   const [list, dispatch] = useReducer(listReducer, []);
+  const [addField, setAddField] = useState('');
 
-  const handleAddClick = () => {
+  const handleAddButton = () => {
+    if(addField.trim() === '') return false;
+
     dispatch({
       type: 'add',
       payload: {
-        text: 'Novo item'
+        text: addField.trim()
       }
-    });
-  }
+    })
 
-  const toggleItemClick = (id: number) =>{
+    setAddField('');
+  };
+
+  const handleDoneCheckbox = (id: number) => {
     dispatch({
       type: 'toggleDone',
-      payload: {
-        id
-      }
+      payload: { id }
     });
-  }
-
-  const editItemClick = (id:number, newText: string) =>{
-    dispatch({
-      type: 'editText',
-      payload: {
-        id,
-        newText
-      }
-    });
-  }
-
-  const removeItemClick = (id: number) =>{
-    dispatch({
-      type: 'remove',
-      payload: {
-        id
-      }
-    });
-  }
-
+  };
 
   return(
-    <div className="">
-      <button onClick={handleAddClick}></button>
+    <div className="container mx-auto">
+      <h1 className="text-center text-4xl my-4">Lista de tarefas</h1>
+      <div className="max-w-2xl mx-auto flex rounded-md bg-gray-900 border border-gray-400 p-4 my-4">
+        <input 
+        placeholder="Digite um item" 
+        className="flex-1 rounded-md border border-white p-3 bg-transparent text-white outline-none mr-3" 
+        type="text" 
+        value={addField}
+        onChange={e => setAddField(e.target.value)}
+        />
+
+        <button 
+        className="p-4 rounded-md bg-blue-700"
+        onClick={handleAddButton}
+        >ADICIONAR</button>
+      </div>
+      <ul className="max-w-2xl mx-auto">
+        {list.map(item => (
+          <li 
+          key={item.id}
+          className="flex items-center p-3 my-3 border-b border-gray-700"
+          >
+            <input type="checkbox" 
+            className="w-6 h-6 mr-4"
+            checked={item.done}
+            onClick={()=> handleDoneCheckbox(item.id)}
+            />
+            <p className="flex-1 text-lg">{item.text}</p>
+            <button className="rounded-md bg-blue-700 mx-4 hover:text-gray-500">Editar</button>
+            <button className="rounded-md bg-blue-700 mx-4 hover:text-gray-500">Excluir</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
