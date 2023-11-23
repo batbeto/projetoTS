0ea32dc1d4a706bbcaf169eb4aad01bd6c3bee0d
Adalberto <btowmaia@gmail.com>
2023-11-23 08:43:34 -0300

    tailwind

--- dark_theme/src/components/Button.tsx
+++ dark_theme/src/components/Button.tsx
@@ -1,15 +1,12 @@
-import { useTheme } from "@/contexts/ThemeContext";
-
 type Props = {
     label: string;
     onClick: () => void;
 }
 
 export const Button = ({ label, onClick }: Props) =>{
-    const themeCtx = useTheme();
     return(
         <button
-            className={`border px-4 py-3 rounded-md ${themeCtx?.theme === 'dark' ? 'bg-white text-black' : 'bg-black text-white'}`}
+            className="border px-4 py-3 rounded-md bg-black text-white dark:bg-white dark:text-black"
             onClick={onClick}
         >
             {label}
