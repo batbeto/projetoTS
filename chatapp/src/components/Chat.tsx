8ed8e906f26e1ab66559aba6b825654198bd191c
Adalberto <btowmaia@gmail.com>
2023-11-29 10:29:09 -0300

    chat v1

--- chatapp/src/components/Chat.tsx
+++ chatapp/src/components/Chat.tsx
@@ -1,4 +1,12 @@
+import { useUser } from "@/contexts/UserContext";
+import { NameInput } from "./NameInput";
+
 export const Chat = () =>{
+    const userCtx = useUser();
+
+    if(!userCtx) return null;
+    if(!userCtx.user) return <NameInput />
+
     return(
         <div className="border border-white/30 rounded-md">
             ...
