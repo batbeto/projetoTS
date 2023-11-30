7cddf5678f484cc1668be442d8566d9abe46745e
Adalberto <btowmaia@gmail.com>
2023-11-30 09:09:16 -0300

    chat

--- chatapp/src/components/Chat.tsx
+++ chatapp/src/components/Chat.tsx
@@ -9,7 +9,15 @@ export const Chat = () =>{
 
     return(
         <div className="border border-white/30 rounded-md">
-            ...
+            <div className="h-96 p-3 overflow-y-scroll">
+                ,...
+            </div>
+            <div className="border-t border-t-white/30 p-3">
+                ...
+            </div>
+            <div className="border-t border-t-white/30 p-3">
+                ;;;;
+            </div>
         </div>
     );
 }
